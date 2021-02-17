6349b6627ca9813b813738f202fed85163d889ce
Robbie <robbiewoolterton@gmail.com>
2021-02-17 16:25:59 +0000

    Opening sensor files now working

--- Robotics Systems/main.cpp
+++ Robotics Systems/main.cpp
@@ -6,10 +6,23 @@
 //  Copyright © 2021 Robbie Woolterton. All rights reserved.
 //
 
-#include <iostream>
+#include <iostream> // used for outputting to the console
+#include "Sensor_Stream.h" // gives us access to the classes declared in other files
+#include "Logger.h"
+#include "Control_Output.h"
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
+
+
+int main(int argc, const char * argv[])
+{
 	std::cout << "Hello, World!\n";
+	
+	
+	
+	Sensor_Stream Sensor1(Sensor_Stream::SENSOR_2);
+
+	
+
+	
 	return 0;
 }
