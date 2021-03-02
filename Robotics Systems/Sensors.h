be556f5a496588318f067c961443ef13a43a69c9
Robbie <robbiewoolterton@gmail.com>
2021-03-02 12:29:56 +0000

    Changed from .hpp to .h

--- Robotics Systems/Sensors.h
+++ Robotics Systems/Sensors.h
@@ -1,8 +1,9 @@
 //
-//  Sensors.h
-//  
+//  Sensors.hpp
+//  Robotics Systems
 //
 //  Created by Robbie Woolterton on 01/03/2021.
+//  Copyright © 2021 Robbie Woolterton. All rights reserved.
 //
 
 #ifndef Sensors_h
@@ -11,8 +12,6 @@
 #include <stdio.h>
 #include "Sensor_Stream.h"
 
-#endif /* Sensors_hpp */
-
 class Sensors
 {
 private:
@@ -20,9 +19,37 @@ private:
 	Sensor_Stream *pSensor2;
 	Sensor_Stream *pSensor3;
 	
+	enum Sensor_Stream::flagState_t state1 = Sensor_Stream::FLAG_DATA_GOOD;
+	enum Sensor_Stream::flagState_t state2 = Sensor_Stream::FLAG_DATA_GOOD;
+	enum Sensor_Stream::flagState_t state3 = Sensor_Stream::FLAG_DATA_GOOD;
+	
+	struct values_t
+	{
+		float value;
+		float previous_value = -1;
+		float temporary_value = 0;
+	};
+
+	values_t Sensor1;
+	values_t Sensor2;
+	values_t Sensor3;
+	
 public:
 	Sensors(Sensor_Stream *pSensor1Inject,Sensor_Stream *pSensor2Inject, Sensor_Stream *pSensor3Inject);
-	
 	void get_data();
+	void convert_data();
+	void scale_data();
+	float return_value(Sensor_Stream:: sensorId_t sensor_id);
+	float fuse_data();
 	
+
 };
+
+	
+
+
+#endif /* Sensors_hpp */
+
+
+
+
